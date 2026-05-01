b5b5e69cf914cb2bb3413aa944ca138d1b7dd0d0
ashik <leadskillit@gmail.com>
2026-05-01 21:45:27 +0600

    update

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,14 +2,14 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
+const client = new MongoClient(process.env.MONGO_DB_URL);
 const db = client.db();
 
 export const auth = betterAuth({
      emailAndPassword: { 
     enabled: true, 
   },
-  
+
   database: mongodbAdapter(db, {
     
     client
